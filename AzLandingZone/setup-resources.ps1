6fd780f21d06ca081ea48b945cb3b56d951a1028
devops <devops@acp4eu074hotmail.onmicrosoft.com>
2020-05-05 10:01:59 +0000

    Updated setup-resources.ps1

--- AzLandingZone/setup-resources.ps1
+++ AzLandingZone/setup-resources.ps1
@@ -1,4 +1,4 @@
-Function Setup-resources {
+Function setup-lzResources {
     Write-Host "This script is private and called by public new-AzLandingZone"
 }
-Export-ModuleMember -Function Setup-resources
+Export-ModuleMember -Function setup-lzResources
