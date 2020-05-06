8fe87fe1b459933347849453bb8ad62bcda16960
devops <devops@acp4eu074hotmail.onmicrosoft.com>
2020-05-06 09:37:12 +0000

    Updated New-AzLandingZone.ps1

--- AzLandingZone/functions/New-AzLandingZone.ps1
+++ AzLandingZone/functions/New-AzLandingZone.ps1
@@ -1,5 +1,6 @@
 Function New-AzLandingZone {
-    setup-Resources
-    setup-policy
+    $name = "lzslz"
+    setup-Resources -Name $name
+    setup-policy -Name $name
 }
 Export-ModuleMember -Function New-AzLandingZone
