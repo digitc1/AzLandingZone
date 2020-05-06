ada2751d3a041f6856a76e49ccb6cf7016c73acc
devops <devops@acp4eu074hotmail.onmicrosoft.com>
2020-05-06 09:53:54 +0000

    Updated Update-AzLandingZone.ps1

--- AzLandingZone/functions/Update-AzLandingZone.ps1
+++ AzLandingZone/functions/Update-AzLandingZone.ps1
@@ -1,6 +1,6 @@
-Function New-AzLandingZone {
+Function Update-AzLandingZone {
     $name = "lzslz"
     setup-Resources -Name $name
     setup-policy -Name $name
 }
-Export-ModuleMember -Function New-AzLandingZone
+Export-ModuleMember -Function Update-AzLandingZone
