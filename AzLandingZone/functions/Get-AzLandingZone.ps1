d456cf0f65e178e279c21ae32adb0ea3f96fcbe6
devops <devops@acp4eu074hotmail.onmicrosoft.com>
2020-05-06 07:44:09 +0000

    Updated Get-AzLandingZone.ps1

--- AzLandingZone/functions/Get-AzLandingZone.ps1
+++ AzLandingZone/functions/Get-AzLandingZone.ps1
@@ -19,12 +19,24 @@ Function Get-AzLandingZone {
     # Check resources
     #
     if($lzResourceGroup = Get-AzResourceGroup | Where-Object {$_.ResourceGroupName -Like "$name*"}){
-        Write-Host "Resource group properly configured" -ForegroundColor Green
+        if(Get-AzResourceLock | Where-Object {$_.Name -Like "LandingZoneLock"}){
+            Write-Host "Resource group properly configured" -ForegroundColor Green
+        }
+        else {
+            Write-Host "Resource group not properly configured" -ForegroundColor Yellow
+        }
     }
     else {
         Write-Host "Landing Zone not installed" -ForegroundColor Red
         Write-Host "Run 'New-AzLandingZone' cmdlet to configure the Landing Zone" -ForegroundColor Red
         return 2
+    }
+    
+    if(Get-AzManagementGroup -GroupName "lz-management-group") {
+        Write-Host "Landing Zone management group properly configured" -ForegroundColor Green
+    }
+    else {
+        Write-Host "Landing Zone management group not properly configured" -ForegroundColor Yellow
     }
 
     if($lzStorageAccount = Get-AzStorageAccount -ResourceGroupName $lzResourceGroup.ResourceGroupName) {
@@ -46,6 +58,25 @@ Function Get-AzLandingZone {
         Write-Host "Landing Zone not installed" -ForegroundColor Red
         Write-Host "Run 'New-AzLandingZone' cmdlet to configure the Landing Zone" -ForegroundColor Red
         return 2
+    }
+
+    if($lzLogAnalyticsWorkspace = Get-AzOperationalInsightsWorkspace -ResourceGroupName $lzResourceGroup.ResourceGroupName) {
+        Write-Host "Optional Azure log analytics and Azure Sentinel are properly configured" -ForegroundColor Green
+    }
+    else {
+        Write-Host "Optional Azure log analytics and Azure Sentinel are not configured" -ForegroundColor Yellow
+    }
+
+    if($lzEventHubNamespace = Get-AzEventHubNameSpace -ResourceGroupName $lzResourceGroup.ResourceGroupName | Where-Object {$_.Name -Like "$name*"}) {
+        if((Get-AzEventHub  -ResourceGroupName $lzResourceGroup.ResourceGroupName -Namespace $lzEventHubNamespace.Name | Where-Object {$_.Name -Like "insights-operational-logs"}) -And (Get-AzEventHubAuthorizationRule -ResourceGroupName $lzResourceGroup.ResourceGroupName -Namespace $lzEventHubNamespace.Name | Where-Object {$_.Name -like "landingZoneAccessKey"})){
+            Write-Host "Optional Azure event-hub is properly configured" -ForegroundColor Green
+        }
+        else {
+            Write-Host "Optional Azure event-hub is installed but not properly configured" -ForegroundColor Red
+        }
+    }
+    else {
+        Write-Host "Optional Azure event-hub is not configured" -ForegroundColor Yellow
     }
 }
 Export-ModuleMember -Function Get-AzLandingZone
