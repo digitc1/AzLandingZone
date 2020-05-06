5181aca1ad471f7aca3cab45c897519bcf1375b4
devops <devops@acp4eu074hotmail.onmicrosoft.com>
2020-05-06 12:09:22 +0000

    Updated New-AzLandingZone.ps1

--- AzLandingZone/functions/New-AzLandingZone.ps1
+++ AzLandingZone/functions/New-AzLandingZone.ps1
@@ -1,4 +1,16 @@
 Function New-AzLandingZone {
-    
+    Param(
+        [Parameter(Mandatory=$true)]
+        [string] $name
+        [ValidateSet("DIGIT", "CERTEU", "None")]
+        [string] $SOC
+        [Parameter(Mandatory=$true)]
+        [bool] $autoupdate
+    )
+
+    Write-Host "The parameters are as follow"
+    Write-Host "Name= $name"
+    Write-Host "SOC= $SOC"
+    Write-Host "auto-update= $autoupdate"
 }
 Export-ModuleMember -Function New-AzLandingZone
