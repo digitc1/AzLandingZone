8651ed7258eca6f9e0f737c97d1307d8510fc6e3
devops <devops@acp4eu074hotmail.onmicrosoft.com>
2020-05-08 13:00:31 +0000

    Updated setup-Storage.ps1

--- AzLandingZone/functions/internal/setup-Storage.ps1
+++ AzLandingZone/functions/internal/setup-Storage.ps1
@@ -6,7 +6,7 @@ Function setup-Storage {
             Write-Host "Please run setup script before running the policy script"
             return 1;
     }
-    
+
     #
     # Checking if Storage account for Landing Zone Logs exists in the secure Landing Zone resource group
     # If it doesn't exist, create it
@@ -30,6 +30,17 @@ Function setup-Storage {
         Write-Host "No Storage container found for Secure Landing Zone logs"
         Write-Host "Creating new Storage container for the Landing Zone Logs in the Secure Landing Zone"
         New-AzRmStorageContainer -ResourceGroupName $GetResourceGroup.ResourceGroupName -StorageAccountName $GetStorageAccount.StorageAccountName -Name "landingzonelogs" | Out-Null
+    }
+
+        #
+    # Checking immutability policy for Azure storage account
+    # If storage is not immutable, set immutability to 185 days
+    #
+    Write-Host "Checking immutability policy" -ForegroundColor Yellow
+    if(((Get-AzRmStorageContainerImmutabilityPolicy -StorageAccountName $GetStorageAccount.StorageAccountName -ResourceGroupName $GetResourceGroup.ResourceGroupName -ContainerName "landingzonelogs").ImmutabilityPeriodSinceCreationInDays) -eq 0){
+    Write-Host "No immutability policy found for logs container"
+    Write-Host "Creating immutability policy (default 185 days)"
+    Set-AzRmStorageContainerImmutabilityPolicy -ResourceGroupName $GetResourceGroup.ResourceGroupName -StorageAccountName $GetStorageAccount.StorageAccountName -ContainerName "landingzonelogs" -ImmutabilityPeriod 185 | Out-Null
     }
 }
 Export-ModuleMember -Function setup-Storage
