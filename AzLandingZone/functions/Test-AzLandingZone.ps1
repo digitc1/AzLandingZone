3162bc03cc7b170a615d7ab7de33c24dc7cb0573
devops <devops@acp4eu074hotmail.onmicrosoft.com>
2020-05-08 11:54:38 +0000

    Updated Test-AzLandingZone.ps1

--- AzLandingZone/functions/Test-AzLandingZone.ps1
+++ AzLandingZone/functions/Test-AzLandingZone.ps1
@@ -3,16 +3,20 @@ Function Test-AzLandingZone {
     $user = Get-AzAdUser | Where-Object {$_.UserPrincipalName -Like "$username*"}
 
     if($user.Count -ne 1){
-        Write-Host "Impossible to identify the current user. Make sure the display name of your user is unique."
+        Write-Verbose "Impossible to identify the current user. Make sure the display name of your user is unique."
+        return 1
     }
     if(!($subscription = Get-AzSubscription | Where-Object {$_.Name -Like "SecLog*"})){
-        Write-Host "Cannot find SecLog subscription. Make sure you're owner or contributor of SecLog subscription."
+        Write-Verbose "Cannot find SecLog subscription. Make sure you're owner or contributor of SecLog subscription."
+        return 1
     }
     $GetAzRoleAssignment = Get-AzRoleAssignment -scope "/subscriptions/$subscription.Id" -objectId $user.Id | Where-Object {$_.RoleDefinitionName -Like "Contributor" -Or $_.RoleDefinitionName -Like "Owner"}
     if($GetAzRoleAssignment.Count -eq 0) {
-        Write-Host "Cannot find role assignment for SecLog subscription. Make sure you're owner or contributor of SecLog subscription."
+        Write-Verbose "Cannot find role assignment for SecLog subscription. Make sure you're owner or contributor of SecLog subscription."
+        return 1
     }
     
-    Write-Host "Validation successful"
+    Write-Verbose "Validation successful"
+    return 0
 }
 Export-ModuleMember -Function Test-AzLandingZone
