44898813b552122741e30098b7a601f57dcc675c
devops <devops@acp4eu074hotmail.onmicrosoft.com>
2020-05-11 08:39:02 +0000

    Updated Test-AzLandingZone.ps1

--- AzLandingZone/functions/Test-AzLandingZone.ps1
+++ AzLandingZone/functions/Test-AzLandingZone.ps1
@@ -9,11 +9,11 @@ Function Test-AzLandingZone {
         Write-Verbose -Message "Impossible to identify the current user. Make sure the display name of your user is unique."
         return 1
     }
-    if(!($subscription = Get-AzSubscription | Where-Object {$_.Name -Like "SecLog*"})){
+    if(!($subscription = Get-AzSubscription | Where-Object {$_.Name -Like "DIGIT_C1*"})){
         Write-Verbose -Message "Cannot find SecLog subscription. Make sure you're owner or contributor of SecLog subscription."
         return 1
     }
-    $GetAzRoleAssignment = Get-AzRoleAssignment -scope "/subscriptions/$subscription.Id" -objectId $user.Id | Where-Object {$_.RoleDefinitionName -Like "Contributor" -Or $_.RoleDefinitionName -Like "Owner"}
+    $GetAzRoleAssignment = Get-AzRoleAssignment -scope "/subscriptions/$subscription" -objectId $user.Id | Where-Object {$_.RoleDefinitionName -Like "Contributor" -Or $_.RoleDefinitionName -Like "Owner"}
     if($GetAzRoleAssignment.Count -eq 0) {
         Write-Verbose -Message "Cannot find role assignment for SecLog subscription. Make sure you're owner or contributor of SecLog subscription."
         return 1
