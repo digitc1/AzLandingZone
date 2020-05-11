f68d43be4ac819a5d52f7d6f8725497770b541a7
devops <devops@acp4eu074hotmail.onmicrosoft.com>
2020-05-11 14:38:38 +0000

    Updated setup-Lighthouse.ps1

--- AzLandingZone/functions/internal/setup-Lighthouse.ps1
+++ AzLandingZone/functions/internal/setup-Lighthouse.ps1
@@ -4,56 +4,22 @@ Function setup-Lighthouse {
             [string] $SOC
     )
 
-    $delegatedResourceManagementURI = ""
-    $delegatedResourceManagementparametersURI = ""
-    #
-    # Get the list of children for the management group
-    #
-    (Get-AzManagementGroup -GroupName "lz-management-group" -Expand).Children | ForEach-Object {
-            Set-AzContext -SubscriptionId $_.Id | Out-Null
+    $delegatedResourceManagementURI = "https://dev.azure.com/devops0837/6414d3a7-f802-4703-8cd7-9cef7c9a9617/_apis/git/repositories/0707bade-f83f-4a91-bbb6-9a13502def90/items?path=%2FLandingZone%2Ftemplates%2FdelegatedResourceManagement.json&versionDescriptor%5BversionOptions%5D=0&versionDescriptor%5BversionType%5D=0&versionDescriptor%5Bversion%5D=master&resolveLfs=true&%24format=octetStream&api-version=5.0&download=true"
+    $delegatedResourceManagementparametersURI = "https://dev.azure.com/devops0837/6414d3a7-f802-4703-8cd7-9cef7c9a9617/_apis/git/repositories/0707bade-f83f-4a91-bbb6-9a13502def90/items?path=%2FLandingZone%2Ftemplates%2FdelegatedResourceManagement.parameters.json&versionDescriptor%5BversionOptions%5D=0&versionDescriptor%5BversionType%5D=0&versionDescriptor%5Bversion%5D=master&resolveLfs=true&%24format=octetStream&api-version=5.0&download=true"
 
-            #
-            # Set contact email address
-            #
-            Write-Host "Checking that security center notification is set to DIGIT_VIRTUAL_TASK_FORCE@ec.europa.eu" -ForegroundColor Yellow
-            if(!(Get-AzSecurityContact | Where-Object {$_.Email -Like "DIGIT-VIRTUAL-TASK-FORCE@ec.europa.eu"})){
-                    $count = (Get-AzSecurityContact).Count
-                    Set-AzSecurityContact -Name "default$($count+1)" -Email "DIGIT-CLOUD-VIRTUAL-TASK-FORCE@ec.europa.eu" -AlertAdmin -NotifyOnAlert | Out-Null
-            }
-            if($SOC -eq "DIGIT"){
-                Write-Host "Checking that security center notification is set to EC-DIGIT-CSIRC@ec.europa.eu" -ForegroundColor Yellow
-                if(!(Get-AzSecurityContact | Where-Object {$_.Email -Like "EC-DIGIT-CSIRC@ec.europa.eu"})){
-                                $count = (Get-AzSecurityContact).Count
-                                Set-AzSecurityContact -Name "default$($count+1)" -Email "EC-DIGIT-CSIRC@ec.europa.eu" -AlertAdmin -NotifyOnAlert | Out-Null
-                }
-                Write-Host "Checking that security center notification is set to EC-DIGIT-CLOUDSEC@ec.europa.eu" -ForegroundColor Yellow
-                if(!(Get-AzSecurityContact | Where-Object {$_.Email -Like "EC-DIGIT-CLOUDSEC@ec.europa.eu"})){
-                                $count = (Get-AzSecurityContact).Count
-                                Set-AzSecurityContact -Name "default$($count+1)" -Email "EC-DIGIT-CLOUDSEC@ec.europa.eu" -AlertAdmin -NotifyOnAlert | Out-Null
-                }
-            }
-            $param = read-Host "Would you like to setup additional contacts for security alerts (y/n)"
-            while($param -Like "y"){
-                    $param -Like "n"
-                    $tmp = Read-Host "Enter the email address for new contact"
-                    $count = (Get-AzSecurityContact).Count
-                    Set-AzSecurityContact -Name "default$($count+1)" -Email $tmp -AlertAdmin -NotifyOnAlert | Out-Null
-                    $param = Read-Host "Successfully added security contact. Add another (y/n) ?"
-            }
     #
     # Following values have been hard-coded in the parameters file:
     # Security Reader (read access to the security center) and Log analytics reader (Read access to Azure Log Analytics workspace and all logs) to DIGIT-S1
     # Security Reader (read access to the security center) and Log analytics reader (Read access to Azure Log Analytics workspace and all logs) to DIGIT-S2
     #
-        if($SOC -eq "DIGIT"){
-                if(!(Get-AzManagedServicesDefinition | Where-Object {$_.Properties.ManagedByTenantId -Like "3a8968a8-fbcf-4414-8b5c-77255f50f37b"})){
-                        Invoke-WebRequest -Uri $delegatedResourceManagementURI -OutFile $HOME/delegatedResourceManagement.json
-                        Invoke-WebRequest -Uri $delegatedResourceManagementURI -OutFile $HOME/delegatedResourceManagement.parameters.json
-                        New-AzDeployment -Name LightHouse -Location "westeurope" -TemplateFile $HOME/delegatedResourceManagement.json -TemplateParameterFile $HOME/delegatedResourceManagement.parameters.json | Out-Null
-                        Remove-Item -Path $HOME/delegatedResourceManagement.parameters.json
-                        Remove-Item -Path $HOME/delegatedResourceManagement.json
-                        Write-Host "Delegation created for DIGIT S"
-                }
+    if($SOC -eq "DIGIT"){
+        if(!(Get-AzManagedServicesDefinition | Where-Object {$_.Properties.ManagedByTenantId -Like "3a8968a8-fbcf-4414-8b5c-77255f50f37b"})){
+            Invoke-WebRequest -Uri $delegatedResourceManagementURI -OutFile $HOME/delegatedResourceManagement.json
+            Invoke-WebRequest -Uri $delegatedResourceManagementURI -OutFile $HOME/delegatedResourceManagement.parameters.json
+            New-AzDeployment -Name LightHouse -Location "westeurope" -TemplateFile $HOME/delegatedResourceManagement.json -TemplateParameterFile $HOME/delegatedResourceManagement.parameters.json | Out-Null
+            Remove-Item -Path $HOME/delegatedResourceManagement.parameters.json
+            Remove-Item -Path $HOME/delegatedResourceManagement.json
+            Write-Host "Delegation created for DIGIT S"
         }
     }
 }
