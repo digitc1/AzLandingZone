6900161e401f3094621e0e301c9c09304268e543
devops <devops@acp4eu074hotmail.onmicrosoft.com>
2020-05-11 07:14:00 +0000

    Updated setup-Prerequisites.ps1

--- AzLandingZone/functions/internal/setup-Prerequisites.ps1
+++ AzLandingZone/functions/internal/setup-Prerequisites.ps1
@@ -9,10 +9,17 @@ Function setup-Prerequisites {
 
     Write-Host "Checking registration for Microsoft PolicyInsights" -ForegroundColor Yellow
     if(!((Get-AzResourceProvider -ProviderNamespace 'Microsoft.PolicyInsights').RegistrationState[0] -Like "Registered")){
-            Register-AzResourceProvider -ProviderNamespace 'Microsoft.PolicyInsights'
-            while(!((Get-AzResourceProvider -ProviderNamespace 'Microsoft.PolicyInsights').RegistrationState[0] -Like "Registered")){
-                    Start-Sleep -s 10
-            }
+        Register-AzResourceProvider -ProviderNamespace 'Microsoft.PolicyInsights'
+        while(!((Get-AzResourceProvider -ProviderNamespace 'Microsoft.PolicyInsights').RegistrationState[0] -Like "Registered")){
+            Start-Sleep -s 10
+        }
+    }
+    Write-Host "Checking registration for Microsoft Security" -ForegroundColor Yellow
+    if(!((Get-AzResourceProvider -ProviderNamespace 'Microsoft.Security').RegistrationState[0] -Like "Registered")){
+        Register-AzResourceProvider -ProviderNamespace 'Microsoft.Security'
+        while(!((Get-AzResourceProvider -ProviderNamespace 'Microsoft.Security').RegistrationState[0] -Like "Registered")){
+            Start-Sleep -s 10
+        }
     }
 }
 Export-ModuleMember -Function setup-Prerequisites
