c83669aa89dea62a7fe369f61226742eab35a68b
devops <devops@acp4eu074hotmail.onmicrosoft.com>
2020-05-11 11:56:28 +0000

    Updated Update-AzLandingZone.ps1

--- AzLandingZone/functions/Update-AzLandingZone.ps1
+++ AzLandingZone/functions/Update-AzLandingZone.ps1
@@ -1,6 +1,53 @@
+workflow work {
+    parallel {
+        setup-Storage -Name $name
+        setup-LogPipeline -Name $name -SOC $SOC
+        setup-Lighthouse -SOC $SOC
+    }
+}
 Function Update-AzLandingZone {
+    Param(
+        [ValidateSet("DIGIT", "CERTEU", "None")]
+        [string] $SOC,
+        [Parameter(Mandatory=$true)]
+        [bool] $autoupdate,
+        [ValidateSet("westeurope", "northeurope", "francecentral", "germanywestcentral")]
+        [string] $location
+    )
+
+    Set-Item Env:\SuppressAzurePowerShellBreakingChangeWarnings "true"
+
+    if(!(Test-AzLandingZone)){
+        Write-Host "Pre-requisite for Azure LandingZone are not met."
+        Write-Host "Run 'Test-AzLandingZone -verbose' for additional information."
+        exit
+    }
+
+    #
+    # Checking registrations and prerequisites for the Landing Zone
+    # Registration can take few minutes
+    #
+    setup-prerequisites
+
+    #
+    # variables
+    #
+    $subscription = Get-AzSubscription | where-Object {$_.Name -Like "DIGIT_C1*"}
+    $context = Set-AzContext -SubscriptionId $subscription.Id
     $name = "lzslz"
-    setup-Resources -Name $name
+    $locations = (Get-AzLocation).Location
+    $locations += "global"
+    if(!($location) -And !(Get-AzResourceGroup | where-Object {$_.ResourceGroupName -Like "$name*"})){
+        $location = "westeurope"
+    }
+
+    setup-Resources -Name $name -Location $location
+    work
+
+    if($autoupdate -eq $true) {
+        setup-Automation -Name $name
+    }
+
     setup-Policy -Name $name
 }
 Export-ModuleMember -Function Update-AzLandingZone
