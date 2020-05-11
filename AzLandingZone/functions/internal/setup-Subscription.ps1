e1b623ce8500c029335e31dd1a3e54e3fb74fee3
devops <devops@acp4eu074hotmail.onmicrosoft.com>
2020-05-11 14:40:01 +0000

    Updated setup-Subscription.ps1

--- AzLandingZone/functions/internal/setup-Subscription.ps1
+++ AzLandingZone/functions/internal/setup-Subscription.ps1
@@ -1,22 +1,19 @@
 Function setup-Subscription {
-    (Get-AzManagementGroup -GroupName "lz-management-group" -Expand).Children | ForEach-Object {
-        Set-AzContext -SubscriptionId $_.Name | Out-Null
-        Set-AzSecurityPricing -Name "VirtualMachines" -PricingTier "Standard" | Out-Null
-        Set-AzSecurityPricing -Name "SqlServers" -PricingTier "Standard" | Out-Null
-        Set-AzSecurityPricing -Name "AppServices" -PricingTier "Standard" | Out-Null
-        Set-AzSecurityPricing -Name "StorageAccounts" -PricingTier "Standard" | Out-Null
-        Set-AzSecurityPricing -Name "KubernetesService" -PricingTier "Standard" | Out-Null
-        Set-AzSecurityPricing -Name "SqlServerVirtualMachines" -PricingTier "Standard" | Out-Null
-        Set-AzSecurityPricing -Name "ContainerRegistry" -PricingTier "Standard" | Out-Null
-        Set-AzSecurityPricing -Name "KeyVaults" -PricingTier "Standard" | Out-Null
+    Set-AzSecurityPricing -Name "VirtualMachines" -PricingTier "Standard" | Out-Null
+    Set-AzSecurityPricing -Name "SqlServers" -PricingTier "Standard" | Out-Null
+    Set-AzSecurityPricing -Name "AppServices" -PricingTier "Standard" | Out-Null
+    Set-AzSecurityPricing -Name "StorageAccounts" -PricingTier "Standard" | Out-Null
+    Set-AzSecurityPricing -Name "KubernetesService" -PricingTier "Standard" | Out-Null
+    Set-AzSecurityPricing -Name "SqlServerVirtualMachines" -PricingTier "Standard" | Out-Null
+    Set-AzSecurityPricing -Name "ContainerRegistry" -PricingTier "Standard" | Out-Null
+    Set-AzSecurityPricing -Name "KeyVaults" -PricingTier "Standard" | Out-Null
 
-        #
-        # Set auto-provisionning for Azure Security Center agents
-        #
-        Write-Host "Checking that security center is enabled and auto-provisioning is working" -ForegroundColor Yellow
-        if(!((Get-AzSecurityAutoProvisioningSetting -Name "default").AutoProvision -Like "On")){
-                Set-AzSecurityAutoProvisioningSetting -Name "default" -EnableAutoProvision | Out-Null
-        }
+    #
+    # Set auto-provisionning for Azure Security Center agents
+    #
+    Write-Host "Checking that security center is enabled and auto-provisioning is working" -ForegroundColor Yellow
+    if(!((Get-AzSecurityAutoProvisioningSetting -Name "default").AutoProvision -Like "On")){
+            Set-AzSecurityAutoProvisioningSetting -Name "default" -EnableAutoProvision | Out-Null
     }
 }
 Export-ModuleMember -Function setup-Subscription
