9c4a86097f01e4b0753200d6b0408ed2ea9a8029
devops <devops@acp4eu074hotmail.onmicrosoft.com>
2020-05-11 14:23:57 +0000

    Updated setup-Policy.ps1

--- AzLandingZone/functions/internal/setup-Policy.ps1
+++ AzLandingZone/functions/internal/setup-Policy.ps1
@@ -194,29 +194,5 @@ Function setup-Policy {
             Write-Host "Created role assignment for: "$policyAssignment.Name
         }
     }
-
-    #
-    # Check if the account is registered to use the Azure Security Center
-    # If not, register
-    #
-    (Get-AzManagementGroup -GroupName "lz-management-group" -Expand).Children | ForEach-Object {
-            Set-AzContext -SubscriptionId $_.Name | Out-Null
-            Set-AzSecurityPricing -Name "VirtualMachines" -PricingTier "Standard" | Out-Null
-            Set-AzSecurityPricing -Name "SqlServers" -PricingTier "Standard" | Out-Null
-            Set-AzSecurityPricing -Name "AppServices" -PricingTier "Standard" | Out-Null
-            Set-AzSecurityPricing -Name "StorageAccounts" -PricingTier "Standard" | Out-Null
-            Set-AzSecurityPricing -Name "KubernetesService" -PricingTier "Standard" | Out-Null
-            Set-AzSecurityPricing -Name "SqlServerVirtualMachines" -PricingTier "Standard" | Out-Null
-            Set-AzSecurityPricing -Name "ContainerRegistry" -PricingTier "Standard" | Out-Null
-            Set-AzSecurityPricing -Name "KeyVaults" -PricingTier "Standard" | Out-Null
-
-            #
-            # Set auto-provisionning for Azure Security Center agents
-            #
-            Write-Host "Checking that security center is enabled and auto-provisioning is working" -ForegroundColor Yellow
-            if(!((Get-AzSecurityAutoProvisioningSetting -Name "default").AutoProvision -Like "On")){
-                    Set-AzSecurityAutoProvisioningSetting -Name "default" -EnableAutoProvision
-            }
-    }
 }
 Export-ModuleMember -Function setup-Policy
