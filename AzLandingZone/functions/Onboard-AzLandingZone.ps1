f53e70e4369dbb32cdf7e5136a90ea6deddf3c85
devops <devops@acp4eu074hotmail.onmicrosoft.com>
2020-05-12 08:18:36 +0000

    Updated Onboard-AzLandingZone.ps1

--- AzLandingZone/functions/Onboard-AzLandingZone.ps1
+++ AzLandingZone/functions/Onboard-AzLandingZone.ps1
@@ -1,27 +1,29 @@
 Function Onboard-AzLandingZone {
-    $children = (Get-AzManagementGroup -GroupName "lz-management-group" -Expand).Children
-    Get-AzSubscription | ForEach-Object {
-        if ($_.Name -notin $children.DisplayName){
-            if($_.Name -Like "SecLog*"){
-                New-AzManagementGroupSubscription -GroupName "lz-management-group" -SubscriptionId $_.Id
-            }
-            else{
-                Write-Host "Do you want to onboard the following subscription: " $_.Name
-                $param = read-Host "enter y or n (default No)"
-                if($param -Like "y"){
-                    Write-Host "Onboarding the subscription"
-                    New-AzManagementGroupSubscription -GroupName "lz-management-group" -SubscriptionId $_.Id
-                    Write-Host "The following subscription is now part of the Landing Zone: "$_.Name
-                }
-            }
-        }
+    Param(
+        [Parameter(Mandatory=$true)][string]$subscription,
+        [ValidateSet("DIGIT", "CERTEU", "None", "")][string] $SOC,
+    )
+
+    #
+    # Onboard the specified subscription
+    #
+
+    if(!($GetSubscription = Get-AzSubscription | Where-Object {$_.Name -Like "$subscription"})){
+        Write-Host "Provided subscription is invalid. Make sure to provide a valid subscription name."
+        return
     }
-    
-    (Get-AzManagementGroup -GroupName "lz-management-group" -Expand).Children | ForEach-Object {
-        Set-AzContext -SubscriptionId $_.Name | Out-Null
-        setup-Subscription
-        setup-SubscriptionContacts
-        setup-Lighthouse
+    if(!($GetManagementGroup = Get-AzManagementGroup -GroupName "lz-management-group" -Expand)){
+        Write-Host "No management group found. Make sure the Landing Zone is installed."
     }
+
+    if($GetSubscription.Name -notin $GetManagementGroup.Children.DisplayName){
+        New-AzManagementGroupSubscription -GroupName "lz-management-group" -SubscriptionId $GetSubscription.Id | Out-Null
+    }
+
+    Set-AzContext -SubscriptionId $GetSubscription.Name | Out-Null
+    setup-Subscription
+    setup-SubscriptionContacts -SOC $SOC
+    setup-Lighthouse -SOC $SOC
+
 }
 Export-ModuleMember -Function Onboard-AzLandingZone
