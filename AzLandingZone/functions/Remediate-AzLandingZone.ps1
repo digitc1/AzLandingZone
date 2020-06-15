d96b8e2f7ebf3e62ab02e253fbcd81190e413f7a
Augustin Colle <augustin.colle@outlook.com>
2020-06-15 09:49:18 +0000

    Updated Remediate-AzLandingZone.ps1

--- AzLandingZone/functions/Remediate-AzLandingZone.ps1
+++ AzLandingZone/functions/Remediate-AzLandingZone.ps1
@@ -1,7 +1,7 @@
 Function Remediate-AzLandingZone{
     $assignmentIdList = (Get-AzPolicyState | Where-Object {$_.PolicyAssignmentName -Like "SLZ-*" -And $_.ComplianceState -Like "NonCompliant"}).PolicyAssignmentId | Sort-Object | Get-Unique
     foreach ($assignmentId in $assignmentIdList){
-            $assignmentName = $assignmentId.split("/")[8]
+            $assignmentName = $assignmentId.split("/") | Select-Object -Last 1
             Start-AzPolicyRemediation -Name "myRemedation_$assignmentName" -PolicyAssignmentId $assignmentId
     }
 }
