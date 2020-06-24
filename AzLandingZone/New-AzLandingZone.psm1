70a0b03b37a69bf052bc98098b366de4e122198f
Augustin Colle <augustin.colle@outlook.com>
2020-06-24 08:37:06 +0000

    Updated New-AzLandingZone.psm1

--- AzLandingZone/New-AzLandingZone.psm1
+++ AzLandingZone/New-AzLandingZone.psm1
@@ -1,17 +1,12 @@
-$module = Get-Module Az.Accounts 
-if ($module -ne $null -and $module.Version.ToString().CompareTo("1.9.0") -lt 0) 
-{
-	Update-Module -Name Az.Accounts -RequiredVersion "1.9.0" -Force
-} 
 if(!(Get-Module AzSentinel)){
-	Install-Module -Force -Name "AzSentinel"
+	Install-Module -Force -Name "AzSentinel" -RequiredVersion "0.6.3"
 	Import-Module AzSentinel
 }
 if(!(Get-Module Az.Security)){
-	Install-Module -Force -Name "Az.Security"
+	Install-Module -Force -Name "Az.Security" -RequiredVersion "0.7.8"
 	Import-Module Az.Security
 }
 if(!(Get-Module Az.Automation)){
-	Install-Module -Force -Name "Az.Automation"
+	Install-Module -Force -Name "Az.Automation" -RequiredVersion "1.3.6"
 	Import-Module Az.Automation
 }
