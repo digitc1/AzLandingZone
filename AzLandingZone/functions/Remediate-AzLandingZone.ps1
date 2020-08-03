8f5afbe07d392bc33dffb96cfb2f24d33983f11d
Augustin Colle <augustin.colle@outlook.com>
2020-08-03 06:50:56 +0000

    Updated Remediate-AzLandingZone.ps1

--- AzLandingZone/functions/Remediate-AzLandingZone.ps1
+++ AzLandingZone/functions/Remediate-AzLandingZone.ps1
@@ -1,6 +1,6 @@
 Function Remediate-AzLandingZone{
     Get-AzPolicyState -ManagementGroupName "lz-management-group" | where-Object {$_.PolicyAssignmentName -Like "SLZ-policyGroup*"} | ForEach-Object {
-        Write-Host "Creating remediation for $_.PolicyDefinitionName in $_.PolicyAssignmentName"
+        Write-Host "Creating remediation for "$_.PolicyDefinitionName
         Start-AzPolicyRemediation -Name $_.PolicyDefinitionName -PolicyAssignmentId $_.PolicyAssignmentId -PolicyDefinitionReferenceId $_.PolicyDefinitionReferenceId | Out-Null
     }
     Get-AzPolicyState -ManagementGroupName "lz-management-group" | where-Object {$_.PolicyAssignmentName -like "SLZ-*"} | where-Object {$_.PolicyAssignmentName -notlike "SLZ-policyGroup*"} | where-Object {$_.ComplianceState -Like "NonCompliant"} | ForEach-Object {
