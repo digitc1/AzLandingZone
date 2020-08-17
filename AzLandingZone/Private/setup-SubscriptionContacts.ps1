6f361bb14dd51d09b652bb388b84feec786f2eb1
augustincolle-digit <67576902+augustincolle-digit@users.noreply.github.com>
2020-08-17 14:28:59 +0200

    Update setup-SubscriptionContacts.ps1 (#6)
    * Update setup-SubscriptionContacts.ps1
    
    * Update AzLandingZone.psd1
    
    Add the changes to AzLandingZone.psd1 to avoid issue when importing unregistered script
    
    * Update pipeline.yml
    
    Updated patch number to allow nuget push
    
    Co-authored-by: augustin <augustin.colle@accenture.com>
    Co-authored-by: silavjy <67747632+silavjy@users.noreply.github.com>

--- AzLandingZone/Private/setup-SubscriptionContacts.ps1
+++ AzLandingZone/Private/setup-SubscriptionContacts.ps1
@@ -1,22 +1,17 @@
-#
-# Breaking changes to this function
-# The function will now deploy a policy (deployifnotexist) that checks the security contacts
-# This function no longer deploys the contacts by it's own
-#
-
 Function setup-SubscriptionContacts {
     param(
         [ValidateSet("DIGIT", "CERTEU", "None", "")]
         [string] $SOC,
         [String[]]$securityContacts
     )
+    $version = 1
 
     #
     # External resources
     #
     $definitionSecurityCenterContactURI = "https://raw.githubusercontent.com/digitc1/AzLandingZonePublic/master/definitions/securityCenter/definition-securityCenterContact.json"
     $securityCenterContactParametersURI = "https://raw.githubusercontent.com/digitc1/AzLandingZonePublic/master/definitions/securityCenter/definition-securityCenterContact.parameters.json"
-
+    
     if(!($GetManagementGroup = Get-AzManagementGroup -GroupName "lz-management-group" -Expand | Where-Object {$_.Name -Like "lz-management-group"})){
             Write-Host "No Management group found for Secure Landing Zone"
             Write-Host "Please run setup script before running the policy script"
@@ -24,38 +19,44 @@ Function setup-SubscriptionContacts {
     }
     $scope = $GetManagementGroup.Id
 
-    Invoke-WebRequest -Uri $definitionSecurityCenterContactURI -OutFile $HOME/rule.json
-    Invoke-WebRequest -Uri $securityCenterContactParametersURI -OutFile $HOME/parameters.json
-
-    if(!($policyDefinition = Get-AzPolicyDefinition -ManagementGroupName "lz-management-group" | Where-Object {$_.Name -Like "SLZ-securityContact"})){
-        $policyDefinition = New-AzPolicyDefinition -Name "SLZ-securityContact" -Policy $HOME/rule.json -Parameter $HOME/parameters.json -ManagementGroupName "lz-management-group"
+    #
+    # Checking definition
+    #
+    if($policyDefinition = Get-AzPolicyDefinition -ManagementGroupName "lz-management-group" | Where-Object {$_.Name -Like "SLZ-securityContact"}){
+        if(!($policyDefinition.Properties.metadata.version -eq $version)){
+            Invoke-WebRequest -Uri $definitionSecurityCenterContactURI -OutFile $HOME/rule.json
+            Invoke-WebRequest -Uri $securityCenterContactParametersURI -OutFile $HOME/parameters.json
+            $metadata = '{"version":"' + $version + '"}'
+            $policyDefinition = Set-AzPolicyDefinition -Id $policyDefinition.ResourceId -Policy $HOME/$policyName.json -Metadata $metadata
+            Remove-Item -Path $HOME/rule.json
+            Remove-Item -Path $HOME/parameters.json
+        }
+    }
+    else{
+        Invoke-WebRequest -Uri $definitionSecurityCenterContactURI -OutFile $HOME/rule.json
+        Invoke-WebRequest -Uri $securityCenterContactParametersURI -OutFile $HOME/parameters.json
+        $metadata = '{"version":"' + $version + '"}'
+        $policyDefinition = New-AzPolicyDefinition -Name "SLZ-securityContact" -Policy $HOME/rule.json -Parameter $HOME/parameters.json -ManagementGroupName "lz-management-group" -Metadata $metadata
+        Remove-Item -Path $HOME/rule.json
+        Remove-Item -Path $HOME/parameters.json
     }
 
-    Write-Host "Checking registration for DIGIT-CLOUD-VIRTUAL-TASK-FORCE@ec.europa.eu as security contact" -ForeGroundColor Yellow
-    if(!(Get-AzPolicyAssignment -Scope $scope | Where-Object {$_.Name -Like "SLZ-securityContact1"})){
-        New-AzPolicyAssignment -name "SLZ-securityContact1" -PolicyDefinition $policyDefinition -Scope $scope -AssignIdentity -Location "westeurope" -contactEmail "DIGIT-CLOUD-VIRTUAL-TASK-FORCE@ec.europa.eu" -contactName "default1" | Out-Null
-    }
-
+    #
+    # Checking assignment
+    #
+    $params = "DIGIT-CLOUD-VIRTUAL-TASK-FORCE@ec.europa.eu"
     if($SOC -eq "DIGIT"){
-        Write-Host "Checking registration for EC-DIGIT-CSIRC@ec.europa.eu as security contact" -ForeGroundColor Yellow
-        if(!(Get-AzPolicyAssignment -Scope $scope | Where-Object {$_.Name -Like "SLZ-securityContact2"})){
-            New-AzPolicyAssignment -name "SLZ-securityContact2" -PolicyDefinition $policyDefinition -Scope $scope -AssignIdentity -Location "westeurope" -contactEmail "EC-DIGIT-CSIRC@ec.europa.eu" -contactName "default2" | Out-Null
-        }
-        Write-Host "Checking registration for EC-DIGIT-CLOUDSEC@ec.europa.eu as security contact" -ForeGroundColor Yellow
-        if(!(Get-AzPolicyAssignment -Scope $scope | Where-Object {$_.Name -Like "SLZ-securityContact3"})){
-            New-AzPolicyAssignment -name "SLZ-securityContact3" -PolicyDefinition $policyDefinition -Scope $scope -AssignIdentity -Location "westeurope" -contactEmail "EC-DIGIT-CLOUDSEC@ec.europa.eu" -contactName "default3" | Out-Null
-        }
+        $params += ";EC-DIGIT-CSIRC@ec.europa.eu;EC-DIGIT-CLOUDSEC@ec.europa.eu"
     }
-
-    foreach ($contact in $securityContacts)  
+    foreach ($contact in $securityContacts.Split(','))  
     { 
-        if($contact -notin ((Get-AzPolicyAssignment -Scope $scope | where-Object {$_.Name -Like "SLZ-securityContact*"}).Properties.Parameters.contactEmail.value)){
-            $count = (Get-AzPolicyAssignment -Scope $scope | where-Object {$_.Name -Like "SLZ-securityContact*"}).Count
-            New-AzPolicyAssignment -name "SLZ-securityContact$($count+1)" -PolicyDefinition $policyDefinition -Scope $scope -AssignIdentity -Location "westeurope" -contactEmail $contact -contactName "default$($count+1)" | Out-Null
-        }
+        $params += ";$contact"
     }
-
-    Remove-item -Path $HOME/rule.json
-    Remove-item -Path $HOME/parameters.json
+    if($policyAssignment = Get-AzPolicyAssignment -Scope "/providers/Microsoft.Management/managementGroups/lz-management-group" | Where-Object {$_.Properties.PolicyDefinitionId -eq $def.ResourceId}) {
+        Set-AzPolicyAssignment -Id $policyAssignment.ResourceId -PolicyParametersObject $params | Out-Null
+    }
+    else {
+        New-AzPolicyAssignment  -name "SLZ-securityContact" -PolicyDefinition $policyDefinition -Scope $scope -AssignIdentity -Location "westeurope" -contactEmail $params -contactName "default" | Out-Null
+    }
 }
 Export-ModuleMember -Function setup-SubscriptionContacts
