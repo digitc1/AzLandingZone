f4da3ec27af6b6cd04e80d6027e7c22dd499d56b
augustincolle-digit <augustin.colle@outlook.com>
2023-03-28 10:22:58 +0200

    Fix module initialization

--- AzLandingZone/AzLandingZone.psm1
+++ AzLandingZone/AzLandingZone.psm1
@@ -20,11 +20,15 @@ $moduleList = @(
 Write-Host "Initialization of the AzLandingZone module in progress"
 foreach($module in $moduleList){
 	$mod = Get-Module -Name $module.Name
-    if ($mod -ne $null -and $mod.Version -lt [System.Version]$module.Version) { 
-        Write-Warning "This module requires $($module.Name) version $($module.Version). An earlier version of $($module.Name) is imported in the current PowerShell session. Please open a new session before importing this module. This error could indicate that multiple incompatible versions of the Azure PowerShell cmdlets are installed on your system." -ErrorAction Continue
-    } 
+    if ($mod -ne $null) {
+		if([System.Version]$module.Version -lt $mod.Version) {
+			Write-Host "The module $($module.Name) was successfully loaded"
+		} else {
+			Write-Warning "This module requires $($module.Name) version $($module.Version). An earlier version of $($module.Name) is imported in the current PowerShell session. Please open a new session before importing this module. This error could indicate that multiple incompatible versions of the Azure PowerShell cmdlets are installed on your system." -ErrorAction Continue
+		}
+	} 
     elseif ($mod -eq $null) { 
-        Import-Module Az.Accounts -MinimumVersion $module.Version -Scope Global 
+        Import-Module Az.Accounts -MinimumVersion $module.Version -Scope Global
     }
 }
 Write-Host "Initialization of the AzLandingZone module completed"
