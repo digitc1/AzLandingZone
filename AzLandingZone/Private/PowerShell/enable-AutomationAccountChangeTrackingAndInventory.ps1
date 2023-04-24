456f038c9d77d1cf44215230c84a21510c0969f9
augustincolle-digit <67576902+augustincolle-digit@users.noreply.github.com>
2023-04-24 13:52:58 +0200

    Update enable-AutomationAccountChangeTrackingAndInventory.ps1
    fix error with $name parameter

--- AzLandingZone/Private/PowerShell/enable-AutomationAccountChangeTrackingAndInventory.ps1
+++ AzLandingZone/Private/PowerShell/enable-AutomationAccountChangeTrackingAndInventory.ps1
@@ -2,7 +2,7 @@ Function enable-AutomationAccountChangeTrackinAndInventory {
     [cmdletbinding()]
     
     param(
-        [Parameter(Mandatory=$true)][string]$resourceGroupName,
+        [Parameter(Mandatory=$true)][string]$name,
         [Parameter(Mandatory=$true)][string]$automationAccountName,
         [Parameter(Mandatory=$true)][string]$lawName
     )
@@ -11,8 +11,9 @@ Function enable-AutomationAccountChangeTrackinAndInventory {
     # Guard
     # Get/check if RG exists
     #
-    if(!($resourceGroup = Get-AzResourceGroup -ResourceGroupName "*$resourceGroupName*")){
+    if (!($GetResourceGroup = Get-AzResourceGroup -ResourceGroupName "*$name*")) {
         Write-Error "No Resource Group for Secure Landing Zone found"
+        Write-Error "Please run setup script before running the policy script"
         return;
     }
 
@@ -20,7 +21,7 @@ Function enable-AutomationAccountChangeTrackinAndInventory {
     # Guard
     # Get/check required automation account
     #
-    if(!($automationAccount = Get-AzAutomationAccount -ResourceGroupName $resourceGroupName -Name $automationAccountName)){
+    if(!($automationAccount = Get-AzAutomationAccount -ResourceGroupName $GetResourceGroup.ResourceGroupName -Name $automationAccountName)){
         Write-Error "No Automation Account for Secure Landing Zone found"
         return;
     }   
@@ -31,17 +32,17 @@ Function enable-AutomationAccountChangeTrackinAndInventory {
     # Guard
     # Check if log analytics workspace exists
     #
-    if(!($law = Get-AzOperationalInsightsWorkspace -Name $lawName -ResourceGroupName $resourceGroupName)){
+    if(!($law = Get-AzOperationalInsightsWorkspace -Name $lawName -ResourceGroupName $GetResourceGroup.ResourceGroupName)){
         Write-Error "No Secure Landing Zone Log Analytics Workspace found"
         return;
     }
 
 
     # Link the Log Analytics Workspace with the Automation Account
-    Set-AzOperationalInsightsLinkedService -ResourceGroupName $resourceGroupName -WorkspaceName $lawName -LinkedServiceName Automation -WriteAccessResourceId "/subscriptions/$subscriptionId/resourceGroups/$resourceGroupName/providers/Microsoft.Automation/automationAccounts/$automationAccountName"
+    Set-AzOperationalInsightsLinkedService -ResourceGroupName $GetResourceGroup.ResourceGroupName -WorkspaceName $lawName -LinkedServiceName Automation -WriteAccessResourceId "/subscriptions/$subscriptionId/resourceGroups/$($GetResourceGroup.ResourceGroupName)/providers/Microsoft.Automation/automationAccounts/$automationAccountName"
  
     # Wnable the Change Tracking solution
-    Set-AzOperationalInsightsIntelligencePack -ResourceGroupName $resourceGroupName -WorkspaceName -WorkspaceName -IntelligencePackName "ChangeTracking" -Enabled $true 
+    Set-AzOperationalInsightsIntelligencePack -ResourceGroupName $GetResourceGroup.ResourceGroupName -WorkspaceName -WorkspaceName -IntelligencePackName "ChangeTracking" -Enabled $true 
 
 }
 Export-ModuleMember -Function enable-AutomationAccountChangeTrackinAndInventory
